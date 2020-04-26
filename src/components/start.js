a27e76d62fb5774f4fa37e555f9bba75bd86b02f
yakik <yaki.koren@gmail.com>
2020-04-26 08:15:06 +0300

    added score, refactored

--- src/components/start.js
+++ src/components/start.js
@@ -3,37 +3,47 @@ import './App.css';
 
 
 export function Start() {
-  const [playerLocation, setPlayerLocation] = useState({row:4,col:5});
+  const [gameState, setGameState] = useState({score:0,playerLocation:{row:4,col:5}});
 
   const handlekeydown=(event)=>{
     event.preventDefault()
+    console.log(event.key)
+  let newGameState = {...gameState}
     if (event.key==='i'){
-      setPlayerLocation({row:playerLocation.row-1,col:playerLocation.col})
+      newGameState.playerLocation={row:newGameState.playerLocation.row-1,col:newGameState.playerLocation.col}
     }
     if (event.key==='m'){
-      setPlayerLocation({row:playerLocation.row+1,col:playerLocation.col})
+      newGameState.playerLocation={row:newGameState.playerLocation.row+1,col:newGameState.playerLocation.col}
     }
     if (event.key==='l'){
-      setPlayerLocation({row:playerLocation.row,col:playerLocation.col+1})
+      newGameState.playerLocation={row:newGameState.playerLocation.row,col:newGameState.playerLocation.col+1}
     }
     if (event.key==='j'){
-      setPlayerLocation({row:playerLocation.row,col:playerLocation.col-1})
+      newGameState.playerLocation={row:newGameState.playerLocation.row,col:newGameState.playerLocation.col-1}
     }
+    if (newGameState.playerLocation.row===3 && newGameState.playerLocation.col===3){
+      newGameState.score+=10  
+    }
+    setGameState(newGameState)
   }
 
   let a=[]
   for (let row=0;row<8;row++)
     for (let col=0;col<10;col++){
-      if (row===playerLocation.row && col===playerLocation.col){
+      if (row===gameState.playerLocation.row && col===gameState.playerLocation.col){
         a.push(<div className="player" key={"row:"+row+" col:"+col}>{row+","+col}</div>)
       } else {
         a.push(<div className="empty" key={"row:"+row+" col:"+col}>{row+","+col}</div>)
       }
     }
    
+   
     return (
+      <div>
+        <p>{"score:" + gameState.score} </p>
       <div className="grid-container" tabIndex="0"  onKeyDown={handlekeydown}>
        {a}
+      </div>
       </div>
     )
  

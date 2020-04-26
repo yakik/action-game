7119322458b1b66870098d8bb8318e8545cb116b
yakik <yaki.koren@gmail.com>
2020-04-26 08:34:29 +0300

    added time

--- src/components/start.js
+++ src/components/start.js
@@ -3,48 +3,61 @@ import './App.css';
 
 
 export function Start() {
-  const [gameState, setGameState] = useState({score:0,playerLocation:{row:4,col:5}});
+  const [gameState, setGameState] = useState({ seconds:0, score: 0, playerLocation: { row: 4, col: 5 } });
 
-  const handlekeydown=(event)=>{
+  
+  const addOneSecondToState=(prevGameState)=>{
+    let newGameState = { ...prevGameState }
+    newGameState.seconds+=1
+    return newGameState
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setGameState(addOneSecondToState)
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handlekeydown = (event) => {
     event.preventDefault()
-    console.log(event.key)
-  let newGameState = {...gameState}
-    if (event.key==='i'){
-      newGameState.playerLocation={row:newGameState.playerLocation.row-1,col:newGameState.playerLocation.col}
+    let newGameState = { ...gameState }
+    if (event.key === 'i') {
+      newGameState.playerLocation = { row: newGameState.playerLocation.row - 1, col: newGameState.playerLocation.col }
     }
-    if (event.key==='m'){
-      newGameState.playerLocation={row:newGameState.playerLocation.row+1,col:newGameState.playerLocation.col}
+    if (event.key === 'm') {
+      newGameState.playerLocation = { row: newGameState.playerLocation.row + 1, col: newGameState.playerLocation.col }
     }
-    if (event.key==='l'){
-      newGameState.playerLocation={row:newGameState.playerLocation.row,col:newGameState.playerLocation.col+1}
+    if (event.key === 'l') {
+      newGameState.playerLocation = { row: newGameState.playerLocation.row, col: newGameState.playerLocation.col + 1 }
     }
-    if (event.key==='j'){
-      newGameState.playerLocation={row:newGameState.playerLocation.row,col:newGameState.playerLocation.col-1}
+    if (event.key === 'j') {
+      newGameState.playerLocation = { row: newGameState.playerLocation.row, col: newGameState.playerLocation.col - 1 }
     }
-    if (newGameState.playerLocation.row===3 && newGameState.playerLocation.col===3){
-      newGameState.score+=10  
+    if (newGameState.playerLocation.row === 3 && newGameState.playerLocation.col === 3) {
+      newGameState.score += 10
     }
     setGameState(newGameState)
   }
 
-  let a=[]
-  for (let row=0;row<8;row++)
-    for (let col=0;col<10;col++){
-      if (row===gameState.playerLocation.row && col===gameState.playerLocation.col){
-        a.push(<div className="player" key={"row:"+row+" col:"+col}>{row+","+col}</div>)
+  let a = []
+  for (let row = 0; row < 8; row++)
+    for (let col = 0; col < 10; col++) {
+      if (row === gameState.playerLocation.row && col === gameState.playerLocation.col) {
+        a.push(<div className="player" key={"row:" + row + " col:" + col}>{row + "," + col}</div>)
       } else {
-        a.push(<div className="empty" key={"row:"+row+" col:"+col}>{row+","+col}</div>)
+        a.push(<div className="empty" key={"row:" + row + " col:" + col}>{row + "," + col}</div>)
       }
     }
-   
-   
-    return (
-      <div>
-        <p>{"score:" + gameState.score} </p>
-      <div className="grid-container" tabIndex="0"  onKeyDown={handlekeydown}>
-       {a}
+
+
+  return (
+    <div>
+      <p>{"Score: " + gameState.score + " Time: " + gameState.seconds + " seconds"} </p>
+      <div className="grid-container" tabIndex="0" onKeyDown={handlekeydown}>
+        {a}
       </div>
-      </div>
-    )
- 
+    </div>
+  )
+
 }
